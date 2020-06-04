250df09f40e6389d4e54c9a2eb973f234995298f
lsjjong8 <lsjjong8@naver.com>
2020-06-04 16:43:23 +0900

    .

--- models/user2.js
+++ models/user2.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const user2 = sequelize.define('user2', {
-    user_id: DataTypes.STRING,
-    password: DataTypes.STRING
+    user_id: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {});
   user2.associate = function(models) {
     // associations can be defined here
