ac2a8c8412fe6cb1cb269488cce6d820c245db51
lsjjong8 <lsjjong8@naver.com>
2020-06-08 17:10:52 +0900

    .

--- routes/product.js
+++ routes/product.js
@@ -2,22 +2,36 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models');
 
-router.get('/home', function (req, res, next) {
-    res.render('product/product.ejs', {
-      title: '호텔 예약 - 모두의 호텔리어'
-    });
-  });
+router.get('/search/:id', function (req, res, next) {
+  let session = req.session;
+  let divisionID = req.params.id;
+
+  if(divisionID == 2) {
+    res.render('product/search.ejs', {
+      title: '호텔 예약 - 모두의 호텔리어',
+      session: session
+    });  
+  }
   
-  router.get('/srp', function (req, res, next) {
-    res.render('product/srp', {
-      title: '내주변 예약 - 모두의 호텔리어'
-    });
-  });
+  res.redirect("/");
+});
+
+router.get('/srp', function (req, res, next) {
+  let session = req.session;
   
-  router.get('/earlybird', function (req, res, next) {
-    res.render('product/earlybird', {
-      title: '페이백 - 모두의 호텔리어'
-    });
+  res.render('product/srp', {
+    title: '내주변 예약 - 모두의 호텔리어',
+    session: session
   });
+});
+
+router.get('/earlybird', function (req, res, next) {
+  let session = req.session;
+  
+  res.render('product/earlybird', {
+    title: '페이백 - 모두의 호텔리어',
+    session: session
+  });
+});
 
 module.exports = router;
